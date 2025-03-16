5a374c9e0f2606d894c5e960026e2682d0b1124f
sarapaolucci <paolucci.sara@franchettisalviani.net>
2025-03-16 20:40:04 +0100

    casa2

--- _impiccato_/Program.cs
+++ _impiccato_/Program.cs
@@ -1,26 +1,78 @@
-﻿Console.WriteLine("Benvenuto nel gioco dell'impiccato, scegli il tema tra Stati, Animali ecc");
-string tema = Console.ReadLine();
-if(tema == "Stati")
+﻿Random random = new Random();
+Console.WriteLine("Benvenuto nel gioco dell'impiccato");
+bool t = false;
+string parola = "";
+while(t == false)
 {
-    
+    Console.WriteLine("Scegli tema tra stati, cantanti, film e serie");
+    string tema = Console.ReadLine();
+    tema = tema.ToLower();
+    if (tema == "stati")
+    {
+        string x = "Stati.txt";
+        parola = stampaopzioni(x);
+        t = true;
+    }
+    else if (tema == "serie")
+    {
+        string x = "Serie.txt";
+        parola = stampaopzioni(x);
+        t = true;
+    }
+    else if (tema == "film")
+    {
+        string x = "Film.txt";
+        parola = stampaopzioni(x);
+        t = true;
+    }
+    else if (tema == "cantanti")
+    {
+        string x = "Cantanti.txt";
+        parola = stampaopzioni(x);
+        t = true;
+    }
+    else
+    {
+        Console.WriteLine("Hai sbagliato a scrivere");
+    }
 }
-void stampaopzioni(string x)
+Console.WriteLine(parola);
+string stampaopzioni(string x)
 {
-    string[] lines = File.ReadAllLines("Stati.txt"); // Legge tutte le righe e le mette in un vettore
-    Console.WriteLine("Ora scegli la difficoltà tra facile, medio e difficile");
-    string difficoltà = Console.ReadLine();
-    int i = 0, j = 0;
-    if (difficoltà == "facile")
+    string[] lines = File.ReadAllLines(x); // Legge tutte le righe e le mette in un vettore
+    bool e = false;
+    int i = 0, j = 0, l = 0;
+    while (e == false)
     {
-        j = 5;
+        Console.WriteLine("Scegli la difficoltà, tra facile, medio e difficile");
+        string difficolta = Console.ReadLine();
+        difficolta = difficolta.ToLower();
+        if (difficolta == "facile")
+        {
+            j = 5;
+            l = 9;
+            e = true;
+        }
+        else if (difficolta == "medio")
+        {
+            i = 5;
+            j = 10;
+            l = 8;
+            e = true;
+        }
+        else if (difficolta == "difficile")
+        {
+            i = 10;
+            j = 15;
+            l = 12;
+            e = true;
+        }
+        else
+        {
+            Console.WriteLine("Hai sbagliato a scrivere");
+        }
+        
     }
-    else if (difficoltà == "medio")
-    {
-        i = 5;
-        j = 10;
-    }
-    for (int a = i; a < j; a++)
-    {
-        Console.WriteLine(lines[a]);
-    }
+    int r = random.Next(i, j);
+    return lines[r].Substring(l);
 }
--- _impiccato_/obj/Debug/net6.0/_impiccato_.AssemblyInfo.cs
+++ _impiccato_/obj/Debug/net6.0/_impiccato_.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("_impiccato_")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e146ebc9e9d017634d69eb564138b630a1e156")]
 [assembly: System.Reflection.AssemblyProductAttribute("_impiccato_")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_impiccato_")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
